6e4dcf9f4e2c40b7c0443d75c9fc9ffb03557062
Fu.EK <295517063@qq.com>
2022-06-23 08:00:30 +0800

    提交了一个git-test文件

---
+++ src/git/test/git01.java
@@ -0,0 +1,13 @@
+package git.test;
+
+/**
+ * @author Fu.ErKe
+ * @create 2022/6/23 - 6:43
+ */
+public class git01 {
+    public static void main(String[] args) { //psvm
+        System.out.println("hello git"); //sout
+        System.out.println("hello git");
+        System.out.println("hello git"); // ctl alt ↓
+    }
+}
